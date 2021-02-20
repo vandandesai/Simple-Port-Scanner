f1fafec674a46275a59628ad9cb2a36ee5f10ac9
vandandesai <73501004+vandandesai@users.noreply.github.com>
2021-02-20 12:57:28 +0530

    Update Simple_port_scanner.py

--- Simple_port_scanner.py
+++ Simple_port_scanner.py
@@ -2,17 +2,17 @@
 
 import socket
 
-s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.settimeout(5)
 
-host =input("Enter the IP: ")
-port =int(input("Enter port number: ")) 
+host = input("Enter the IP: ")
+port = int(input("Enter port number: "))
 
 
 def portscanner(port):
-    if s.connect_ex((host , port)):
+    if s.connect_ex((host, port)):
         print("Port is closed")
     else:
         print("Port is open")
 
-portscanner(port) 
+portscanner(port)
